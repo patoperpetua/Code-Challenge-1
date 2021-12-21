7c0decba3083f3645dc0f617b833a0568f36cbff
Patricio Perpetua <patoperpetua@singletonsd.com>
2021-12-21 12:54:56 +1100

    Add Spring App.

---
+++ src/main/java/com/singletonsd/iecodechallenge/IeCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.singletonsd.iecodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeCodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeCodeChallengeApplication.class, args);
+	}
+
+}
